f09ff7af22bc737f0eea0d2196c7656eabc7e705
PG Chen <pgzoe860120@gmail.com>
2024-09-03 13:33:17 +0800

    GBController寫程式碼取值

---
+++ WebApplication3/Controllers/GBController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models.EFModels;
+using WebApplication3.Models.ViewModels;
+
+namespace WebApplication3.Controllers
+{
+    public class GBController : Controller
+    {
+        // GET: GB
+        public ActionResult Index()
+        {
+            var data = GetData();
+            return View(data);
+        }
+
+        private List<GuestBookVm> GetData()
+        {
+            return new AppDbContext().GuestBooks
+                .Select(gb=>new GuestBookVm()
+                {
+                    Id = gb.Id,
+                    Name = gb.Name,
+                    Email= gb.Email,
+                    CreateTime = gb.CreateTime,
+                })
+                .OrderByDescending(gb=>gb.CreateTime)
+                .ToList();
+        }
+    }
+}

e926baf97b14faa3ef971d625b35b073d578e4d8
PG Chen <pgzoe860120@gmail.com>
2024-09-03 13:26:55 +0800

    建ViewModels資料夾，裡面放GuestBookVm和GuestBookCreateVm

---
+++ WebApplication3/Models/ViewModels/GuestBookCreateVm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models.ViewModels
+{
+    public class GuestBookCreateVm
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(254)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string CellPhone { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Message { get; set; }
+
+    }
+}
---
+++ WebApplication3/Models/ViewModels/GuestBookVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models.ViewModels
+{
+    public class GuestBookVm
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(254)]
+        public string Email { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}

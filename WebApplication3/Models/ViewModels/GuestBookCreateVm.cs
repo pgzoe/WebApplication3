5dde9fba396026dac809fabe451e2d9c5e1a148b
PG Chen <pgzoe860120@gmail.com>
2024-09-03 14:37:12 +0800

    修改create留言的欄位標題為中文

--- WebApplication3/Models/ViewModels/GuestBookCreateVm.cs
+++ WebApplication3/Models/ViewModels/GuestBookCreateVm.cs
@@ -6,17 +6,21 @@ namespace WebApplication3.Models.ViewModels
     {
         public int Id { get; set; }
 
+        [Display(Name = "姓名")]
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
 
+        [Display(Name = "電子郵件")]
         [Required]
         [StringLength(254)]
         public string Email { get; set; }
 
+        [Display(Name = "手機")]
         [StringLength(50)]
         public string CellPhone { get; set; }
 
+        [Display(Name = "留言")]
         [Required]
         [StringLength(200)]
         public string Message { get; set; }
